8be1e7e2d4a63f44366aab16b308d847903698fc
Ismail Opatola <opatolamails@gmail.com>
2019-08-04 17:42:41 +0100

    add post component, fecth data from markdown

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,51 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql, StaticQuery } from "gatsby"
+import Post from "../components/Post"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Home page</h1>
+    <StaticQuery
+      query={indexQuery}
+      render={data => {
+        return (
+          <div>
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+              <Post
+                title={node.frontmatter.title}
+                author={node.frontmatter.author}
+                date={node.frontmatter.date}
+                path={node.frontmatter.path}
+                body={node.frontmatter.excerpt}
+              />
+            ))}
+          </div>
+        )
+      }}
+    />
   </Layout>
 )
+
+const indexQuery = graphql`
+  query {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+      edges {
+        node {
+          id
+          excerpt
+          frontmatter {
+            title
+            date(formatString: "MMM Do YYYY")
+            author
+            path
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
