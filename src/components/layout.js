f70f8760fd99a3078b808ba5728d90614ca90a33
Ismail Opatola <opatolamails@gmail.com>
2019-08-04 12:41:17 +0100

    initial commit -- remove default generated files

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,6 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -34,12 +33,7 @@ const Layout = ({ children }) => {
           paddingTop: 0,
         }}
       >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        {children}
       </div>
     </>
   )
