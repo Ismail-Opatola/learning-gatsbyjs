2dad8c8b85635af4a2cf2d8abdf30a331648e034
Ismail Opatola <opatolamails@gmail.com>
2019-08-05 09:29:04 +0100

    use single-post-template to generate url-slug and page for each post

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,58 @@
  */
 
 // You can delete this file if you're not using it
+
+const { slugify } = require("./src/util/utilityFunctions")
+const path = require("path")
+
+// create slug dynamically
+// use title as slug (previously we we're using hardcoded path in the post markdown files)
+exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === "MarkdownRemark") {
+    const slugFromTitle = slugify(node.frontmatter.title)
+    createNodeField({
+      node,
+      name: "slug",
+      value: slugFromTitle,
+    })
+  }
+}
+
+// available after onCreateNode as been executed
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions
+  const singlePostTemplate = path.resolve("src/templates/single-post.js")
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              author
+              tags
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(res => {
+    if (res.errors) return Promise.reject(res.errors)
+    const posts = res.data.allMarkdownRemark.edges
+
+    posts.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: singlePostTemplate,
+        context: {
+          // fetch slug from template, use it to get post
+          slug: node.fields.slug,
+        }
+      })
+    })
+  })
+}
